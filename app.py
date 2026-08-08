108b2034f2a4fb68af099f434b70414ebb1fc50a
habibitrails-ux <habibitrails@gmail.com>
2026-08-09 01:59:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from flask import Flask, request, jsonify, render_template
 from concurrent.futures import ThreadPoolExecutor
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app = Flask(__name__, template_folder='templates')
 
 AI_BOTS = ["GPTBot", "PerplexityBot", "ClaudeBot", "Google-Extended", "ChatGPT-User"]
 
