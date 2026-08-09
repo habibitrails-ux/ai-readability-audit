eabad404f910c46ede9590b343e5533ec0339a69
habibitrails-ux <habibitrails@gmail.com>
2026-08-09 22:55:12 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,4 @@
-import os
-import sys
-
-# Add root directory to path to allow importing app module cleanly
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from app import app
 
-# Vercel Serverless Function Handler
-if __name__ == '__main__':
-    app.run()
+# Entrypoint for Vercel Serverless Function deployment
+app = app
