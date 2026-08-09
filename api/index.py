07425304959a77bf3e034bb8be62968f87c83018
habibitrails-ux <habibitrails@gmail.com>
2026-08-09 13:36:03 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
 from app import app
+
+# Vercel requires the WSGI application callable to be exposed
+app = app
+
+if __name__ == "__main__":
+    app.run()
